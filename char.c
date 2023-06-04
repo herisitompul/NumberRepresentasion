350b8b16a84418c4cdbe44b32c3e57ca43a4187e
herisitompul <135317357+herisitompul@users.noreply.github.com>
2023-06-05 00:39:14 +0700

    char.c

---
+++ char.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char ch = 'A';      // Deklarasi variabel ch dengan karakter 'A'
+    char digit = '7';   // Deklarasi variabel digit dengan karakter '7'
+
+    printf("Karakter: %c\n", ch);
+    printf("Angka: %c\n", digit);
+
+    return 0;
+}
